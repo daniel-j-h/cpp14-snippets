e1911b878f1a518357a0aa5d1acdb916a5fada28
Daniel J. Hofmann <daniel@trvx.org>
2014-09-17 13:33:12 +0200

    Additional generic functor<>{} example

--- functors.cc
+++ functors.cc
@@ -1,6 +1,7 @@
 #include <cassert>
 #include <functional>
 #include <algorithm>
+#include <numeric>
 #include <vector>
 #include <string>
 #include <iterator>
@@ -17,6 +18,14 @@ int main() {
   // no need to write std::greater<std::string>{}
   std::nth_element(std::begin(svec), second, std::end(svec), std::greater<>{});
   assert(*second == "xxx"); // second largest element is xxx
+
+
+  // count_ones([0 1 1 0] | [1 1 0 0]) = count_ones([1 1 1 0]) == 3
+  std::vector<bool> lhs{0, 1, 1, 0}, rhs{1, 1, 1, 0};
+  auto count = std::inner_product(std::begin(lhs), std::end(lhs), std::begin(rhs),
+                                  0, std::plus<>{}, std::logical_or<>{});
+  // functor<>{} instantiates a new functor template with generic operator()
+  assert( count == 3 );
 
   // see: http://en.cppreference.com/w/cpp/utility/functional#Function_objects
 
